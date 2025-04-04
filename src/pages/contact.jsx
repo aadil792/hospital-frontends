339fc89dde3d71a3522c9f4772550e62f350ae0a
aadil792 <aadilmd792@gmail.com>
2025-04-04 10:43:37 +0530

    added contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,15 +1,32 @@
 import Layout from "../componets/layout/layout";
 import "../pages/pages_css/contact.css";
-const Contact_page=()=>{
-    return(
-        <>
-        <Layout>
-
+const Contact_page = () => {
+  return (
+    <>
+      <Layout>
         <div className="contact-main-div">
-            <h1>Contact page</h1>
+          <div className="contact-matter-main">
+            <h2 className="h2-contact-main">Contact Us</h2>
+            <h2 className="h2-contact-h">Hospital Novena</h2>
+            <div className="tags-contact">
+              <h3>📍 Address: [Street Address, Hyderabad,Pin Code: 898732]</h3>
+              <h3>📞 Phone: +91 [98764567777]</h3>
+              <h3>📧 Email: [novena@gmail.com]</h3>
+              <h3>🌐 Website: [https://hospital-frontends.vercel.app/]</h3>
+
+              <h3>
+                🕒 Working Hours: Monday - Saturday: 8:00 AM - 8:00 PM Sunday:
+                9:00 AM - 2:00 PM
+              </h3>
+              <h3>
+                Emergency Contact 🚑 24/7 Emergency Helpline: +91 [Emergency
+                Number]
+              </h3>
+            </div>
+          </div>
         </div>
-        </Layout>
-        </>
-    )
-}
+      </Layout>
+    </>
+  );
+};
 export default Contact_page;
